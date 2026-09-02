ba56813e0bd96566418112c8e8198bedc1935565
Bartosz Szczeciński <54596452+syscore88@users.noreply.github.com>
2026-09-02 15:23:00 +0200

    update

--- install.sh
+++ install.sh
@@ -415,6 +415,52 @@ show_progress 7 $TOTAL_STEPS "$MSG_PHASE_2"
 wait_for_rpm_lock
 sudo dnf5 install -y --skip-unavailable virt-manager qemu-kvm qemu-img libvirt libvirt-daemon-kvm edk2-ovmf dnsmasq || true
 
+dconf load /org/virt-manager/virt-manager/ <<'DCONFEOF'
+[/]
+manager-window-height=297
+manager-window-width=478
+xmleditor-enabled=true
+
+[confirm]
+delete-storage=false
+forcepoweroff=false
+
+[connections]
+autoconnect=['qemu:///system']
+uris=['qemu:///system']
+
+[conns/qemu:system]
+window-size=(800, 600)
+
+[details]
+show-toolbar=true
+
+[new-vm]
+cpu-default='host-passthrough'
+firmware='uefi'
+graphics-type='spice'
+storage-format='raw'
+
+[paths]
+media-default='/home/bartek/Pobrane'
+
+[stats]
+enable-disk-poll=true
+enable-memory-poll=true
+enable-net-poll=true
+
+[urls]
+isos=['/var/lib/libvirt/images/archlinux.img', '/home/bartek/Pobrane/archlinux-2026.09.01-x86_64.iso']
+
+[vmlist-fields]
+disk-usage=false
+network-traffic=false
+
+[vms/2a91721fef6c4249997ea19b01801825]
+autoconnect=1
+vm-window-size=(1280, 842)
+DCONFEOF
+
 for svc in libvirtd virtqemud; do
     if systemctl list-unit-files "$svc.service" &>/dev/null 2>&1 && systemctl list-unit-files "$svc.service" | grep -q "$svc"; then
         sudo systemctl enable --now "$svc.service" || true
