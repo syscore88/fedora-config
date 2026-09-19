a807d34515d65d0e4d38f0aec8010e34fc243c06
Bartosz Szczeciński <54596452+syscore88@users.noreply.github.com>
2026-09-19 02:14:56 +0200

    update

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # ==========================================================
-# KOMPLEKSOWY SKRYPT KONFIGURACYJNY SYSTEMU (FEDORA)
+# KOMPLEKSOWY SKRYPT KONFIGURACYJNY SYSTEMU FEDORA
 # ==========================================================
 
 set -Eeuo pipefail
